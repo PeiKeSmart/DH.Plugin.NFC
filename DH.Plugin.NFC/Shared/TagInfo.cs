9b6874a7f51acf413ed56fee5b6f257f09aa42db
猿人易 <qcjxberin@outlook.com>
2024-09-08 09:29:10 +0800

    添加 NFC 功能支持
    在 `DH.Plugin.NFC.csproj` 文件中：
      * 注释掉 `<ImplicitUsings>enable</ImplicitUsings>`。
      * 在 `<NoWarn>` 属性中添加 `1591` 和 `CS0067`。
      * 注释掉 `Microsoft.Maui.Controls` 包引用。
    
    在 `PlatformClass1.cs` 文件中：
      * 删除 Android 和 iOS 平台特定的 `PlatformClass1` 类。
    
    在 `CrossNFC.cs` 文件中：
      * 添加 `CrossNFC` 类，包含初始化和处理 Android 特定 NFC 功能的方法。
      * 添加 `ActivityLifecycleContextListener` 类，用于监听 Android 活动生命周期回调。
    
    在 `NFC.cs` 文件中：
      * 添加 `NFCImplementation` 类，实现 `INFC` 接口，处理 Android 平台的 NFC 功能。
      * 实现 NFC 标签的读取、写入、格式化和监听功能。
      * 添加 `NfcBroadcastReceiver` 类，用于监听 NFC 状态变化。
    
    添加了以下新文件：
      * `INFC.cs`：定义 NFC 功能的主要接口方法和事件。
      * `ITagInfo.cs`：描述 NFC 标签的信息接口。
      * `NfcConfiguration.cs`：配置 NFC 功能的类。
      * `NFCUtils.cs`：提供 NFC 工具方法的静态类。
      * `TagInfo.cs`：实现 `ITagInfo` 接口，提供 NFC 标签的默认实现。

---
+++ DH.Plugin.NFC/Shared/TagInfo.cs
@@ -0,0 +1,61 @@
+﻿namespace Plugin.NFC;
+
+/// <summary>
+/// Default implementation of <see cref="ITagInfo"/>
+/// </summary>
+public class TagInfo : ITagInfo
+{
+    public byte[] Identifier { get; }
+
+    /// <summary>
+    /// Tag Serial Number
+    /// </summary>
+    public string SerialNumber { get; }
+
+    /// <summary>
+    /// Writable tag
+    /// </summary>
+    public bool IsWritable { get; set; }
+
+    /// <summary>
+    /// Capacity of tag in bytes
+    /// </summary>
+    public int Capacity { get; set; }
+
+    /// <summary>
+    /// Array of <see cref="NFCNdefRecord"/> of tag
+    /// </summary>
+    public NFCNdefRecord[] Records { get; set; }
+
+    /// <summary>
+    /// Empty tag
+    /// </summary>
+    public bool IsEmpty => Records == null || Records.Length == 0 || Records[0] == null || Records[0].TypeFormat == NFCNdefTypeFormat.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool IsSupported { get; private set; }
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public TagInfo()
+    {
+        IsSupported = true;
+    }
+
+    /// <summary>
+    /// Custom contructor
+    /// </summary>
+    /// <param name="identifier">Tag Identifier</param>
+    /// <param name="isNdef">Is Ndef tag</param>
+    public TagInfo(byte[] identifier, bool isNdef = true)
+    {
+        Identifier = identifier;
+        SerialNumber = NFCUtils.ByteArrayToHexString(identifier);
+        IsSupported = isNdef;
+    }
+
+    public override string ToString() => $"TagInfo: identifier: {Identifier}, SerialNumber:{SerialNumber}, Capacity:{Capacity} bytes, IsSupported:{IsSupported}, IsEmpty:{IsEmpty}, IsWritable:{IsWritable}";
+}
