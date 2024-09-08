ccf5208d16031160130bc07dd8460a852fb58308
猿人易 <qcjxberin@outlook.com>
2024-09-08 16:49:53 +0800

    新增 Transceive 方法及其异步版本
    新增了两个方法 `Transceive` 和 `TransceiveAsync`，用于发送原始NFC命令，并处理标签连接、数据传输和异常。`Transceive` 方法接受 `NfcType` 和 `data` 两个参数，返回一个字节数组。`TransceiveAsync` 方法支持异步操作。
    
    此外，将现有代码中的 `NdefMessage` 声明修改为 `NdefMessage?`，以支持可空类型。

--- DH.Plugin.NFC/Platforms/Android/NFC.cs
+++ DH.Plugin.NFC/Platforms/Android/NFC.cs
@@ -162,6 +162,136 @@ public class NFCImplementation : INFC
     public void PublishMessage(ITagInfo tagInfo, bool makeReadOnly = false) => WriteOrClearMessage(tagInfo, false, makeReadOnly);
 
     /// <summary>
+    /// 发送原始命令
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="NfcType">Nfc类型，如NfcA</param>
+    public byte[]? Transceive(String NfcType, byte[] data)
+    {
+        byte[]? result = null;
+
+        try
+        {
+            if (_currentTag == null)
+                throw new Exception(Configuration.Messages.NFCErrorMissingTag);
+
+            switch (NfcType)
+            {
+                case "NfcA":
+                    var ndef = NfcA.Get(_currentTag);
+                    if (ndef != null)
+                    {
+                        try
+                        {
+                            ndef.Connect();
+                            OnTagConnected?.Invoke(null, EventArgs.Empty);
+
+                            result = ndef.Transceive(data);
+                        }
+                        catch (Android.Nfc.TagLostException tlex)
+                        {
+                            throw new Exception("Tag Lost Error: " + tlex.Message);
+                        }
+                        catch (Java.IO.IOException ioex)
+                        {
+                            throw new Exception("Tag IO Error: " + ioex.Message);
+                        }
+                        catch (Android.Nfc.FormatException fe)
+                        {
+                            throw new Exception("Tag Format Error: " + fe.Message);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception("Tag Error:" + ex.Message);
+                        }
+                        finally
+                        {
+                            if (ndef.IsConnected)
+                                ndef.Close();
+
+                            OnTagDisconnected?.Invoke(null, EventArgs.Empty);
+                        }
+                    }
+                    else
+                        throw new Exception(Configuration.Messages.NFCErrorNotCompliantTag);
+
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            StopPublishingAndThrowError(ex.Message);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// 发送原始命令
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="NfcType">Nfc类型，如NfcA</param>
+    public async Task<byte[]?> TransceiveAsync(String NfcType, byte[] data)
+    {
+        byte[]? result = null;
+
+        try
+        {
+            if (_currentTag == null)
+                throw new Exception(Configuration.Messages.NFCErrorMissingTag);
+
+            switch (NfcType)
+            {
+                case "NfcA":
+                    var ndef = NfcA.Get(_currentTag);
+                    if (ndef != null)
+                    {
+                        try
+                        {
+                            ndef.Connect();
+                            OnTagConnected?.Invoke(null, EventArgs.Empty);
+
+                            result = await ndef.TransceiveAsync(data);
+                        }
+                        catch (Android.Nfc.TagLostException tlex)
+                        {
+                            throw new Exception("Tag Lost Error: " + tlex.Message);
+                        }
+                        catch (Java.IO.IOException ioex)
+                        {
+                            throw new Exception("Tag IO Error: " + ioex.Message);
+                        }
+                        catch (Android.Nfc.FormatException fe)
+                        {
+                            throw new Exception("Tag Format Error: " + fe.Message);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception("Tag Error:" + ex.Message);
+                        }
+                        finally
+                        {
+                            if (ndef.IsConnected)
+                                ndef.Close();
+
+                            OnTagDisconnected?.Invoke(null, EventArgs.Empty);
+                        }
+                    }
+                    else
+                        throw new Exception(Configuration.Messages.NFCErrorNotCompliantTag);
+
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            StopPublishingAndThrowError(ex.Message);
+        }
+
+        return result;
+    }
+
+    /// <summary>
     /// Format tag
     /// </summary>
     /// <param name="tagInfo">see <see cref="ITagInfo"/></param>
@@ -197,7 +327,7 @@ public class NFCImplementation : INFC
                     ndef.Connect();
                     OnTagConnected?.Invoke(null, EventArgs.Empty);
 
-                    NdefMessage message = null;
+                    NdefMessage? message = null;
                     if (clearMessage)
                     {
                         message = GetEmptyNdefMessage();
